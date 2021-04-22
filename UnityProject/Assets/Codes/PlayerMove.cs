6abd0ad1cec4056b63d78d4c9c538182c50c1745
NooB <oleg.nakhaevgame@mail.ru>
2021-04-23 01:38:15 +0300

    Homework2

--- UnityProject/Assets/Codes/PlayerMove.cs
+++ UnityProject/Assets/Codes/PlayerMove.cs
@@ -2,27 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
+namespace Game
 {
-    [SerializeField] private float Force;
-
-    private Rigidbody _rb;
-    private float _vertical;
-    private float _horizontal;
-    private Vector3 _move;
-
-    private void Start()
+    public class PlayerMove : MonoBehaviour
     {
-        _rb = GetComponent<Rigidbody>();
+        public float Force;
+
+        private Rigidbody _rb;
+        private float _vertical;
+        private float _horizontal;
+        private Vector3 _move;
+
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            _vertical = Input.GetAxis("Vertical");
+            _horizontal = Input.GetAxis("Horizontal");
+
+            _move = new Vector3(_horizontal, 0, _vertical);
+            _rb.AddForce(_move * Force);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.gameObject.CompareTag("Bonus"))
+            {
+               collision.gameObject.GetComponent<IBonus>().Bonus();
+            }
+
+            if(collision.gameObject.CompareTag("Trap"))
+            {
+                collision.gameObject.GetComponent<ITraps>().Trap();
+            }
+        }
     }
-
-    private void FixedUpdate()
-    {
-        _vertical = Input.GetAxis("Vertical");
-        _horizontal = Input.GetAxis("Horizontal");
-
-        _move = new Vector3(_horizontal, 0, _vertical);
-        _rb.AddForce(_move * Force);
-    }
-
 }
